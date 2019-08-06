d3e6f3c7c84879607a9365e508ae7f74e115ada9
dineshdsv3 <dineshdsv3@gmail.com>
2019-08-07 00:50:10 +0530

    re added backend folder

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var authRouter = require("./routes.js");
+
+app.use(authRouter);
+
+app.use(express.static("../Public"));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve("../Public/html/index.html"))
+})
+
+app.listen('3000',() =>{
+    console.log("Server Started on port 3000");
+})
+
+module.exports = app;
